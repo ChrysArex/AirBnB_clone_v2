d05ab4a68779e842eb0e9f8c6388103f3855cfb4
root <root@localhost.localdomain>
2023-12-10 23:59:17 +0100

    task-0 done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,40 @@
 #!/usr/bin/env bash
 #sets up your web servers for the deployment of web_static
 
-apt install nginx
-if [ -d "/data/" ]; then
-  mkdir -p "/data/";
+sudo apt install nginx
+if [ ! -d "/data/" ]; then
+  sudo mkdir -p "/data/";
 fi
-if [ -d "/data/web_static/" ]; then
-  mkdir -p "/data/web_static/";
+if [ ! -d "/data/web_static/" ]; then
+  sudo mkdir -p "/data/web_static/";
 fi
-if [ -d "/data/web_static/releases/" ]; then
-  mkdir -p "/data/web_static/releases/";
+if [ ! -d "/data/web_static/releases/" ]; then
+  sudo mkdir -p "/data/web_static/releases/";
 fi
-if [ -d "/data/web_static/shared/" ]; then
-  mkdir -p "/data/web_static/shared/";
+if [ ! -d "/data/web_static/shared/" ]; then
+  sudo mkdir -p "/data/web_static/shared/";
 fi
-if [ -d "/data/web_static/releases/test/" ]; then
-  mkdir -p "/data/web_static/releases/test/";
+if [ ! -d "/data/web_static/releases/test/" ]; then
+  sudo mkdir -p "/data/web_static/releases/test/";
 fi
 
 echo "hello test" > "/data/web_static/releases/test/index.html"
 
-if [ -f "/data/web_static/current" ]; then
-  rm /data/web_static/current;
+if [ -L "/data/web_static/current" ]; then
+  sudo rm "/data/web_static/current"
 fi
 
-ln -s "/data/web_static/current" "/data/web_static/releases/test/";
+sudo ln -s "/data/web_static/releases/test/" "/data/web_static/current"
 
-chown ubuntu:ubuntu -R /data/
+sudo chown ubuntu:ubuntu -R /data/
 
-sudo sed -i "http {/a\
-	server {\
-		location /hbnb_static/ {\
-			alias /data/web_static/current/\
-		}\
-	}" /etc/nginx/nginx.conf
+sudo sed -i "/http {/a\
+\n      server {\n\
+                location \/hbnb_static\/ {\n\
+                        alias \/data\/web_static\/current\/;\n\
+                }\n\
+        }" /etc/nginx/nginx.conf
+
+sudo sed -i "s/n      server {/      server {/g" /etc/nginx/nginx.conf
+
+sudo nginx -s reload
