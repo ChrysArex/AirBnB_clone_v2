ce5e83a49930ea29419503a3d6fcc53c400df3ba
root <root@localhost.localdomain>
2024-01-01 22:09:22 +0100

    task 0-6 done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,9 @@ if __name__ == "__main__":
 
     app = Flask(__name__)
 
-
     @app.route("/", strict_slashes=False)
     def hello():
         """display “Hello HBNB!”"""
         return "Hello HBNB!"
-
 
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask
-from markupsafe import escape
+if __name__ == "__main__":
+    from flask import Flask
+    from markupsafe import escape
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
+    @app.route("/", strict_slashes=False)
+    def hello_hbnb():
+        """display “Hello HBNB!”"""
+        return "Hello HBNB!"
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """display “Hello HBNB!”"""
-    return "Hello HBNB!"
+    @app.route("/hbnb", strict_slashes=False)
+    def hello():
+        """display “HBNB”"""
+        return "HBNB"
 
+    @app.route('/c/<text>', strict_slashes=False)
+    def cstring(text):
+        """display “C ” followed by the value of the text variable"""
+        return "C " + escape(text).replace('_', ' ')
 
-@app.route("/hbnb", strict_slashes=False)
-def hello():
-    """display “HBNB”"""
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def cstring(text):
-    """display “C ” followed by the value of the text variable"""
-    return "C " + escape(text).replace('_', ' ')
-
-
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
